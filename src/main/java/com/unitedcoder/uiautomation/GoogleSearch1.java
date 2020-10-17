dd857ad0395dca9a1715fc221a9f1a4fcfceb283
Aynigar <aynigar615@gmail.com>
2020-10-17 13:22:39 +0300

    Tax

--- src/main/java/com/unitedcoder/uiautomation/GoogleSearch1.java
+++ src/main/java/com/unitedcoder/uiautomation/GoogleSearch1.java
@@ -7,18 +7,20 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleSearch1 {
-    public static void main(String[] args) {
-//define chrome driver location
+    public static void main(String[] args) throws InterruptedException {
+        //define chrome driver location
         System.setProperty("webdriver.chrome.driver","c:\\webdriver\\chromedriver.exe");
         //define chrome driver object instance
         WebDriver driver=new ChromeDriver();
         //maximize the browser
         driver.manage().window().maximize();
+
         //open google site
         driver.get("https://www.google.com");
         //find search box element
         WebElement searchBox=driver.findElement(By.name("q"));
-        searchBox.sendKeys("Selenium"+ Keys.ENTER);
+        searchBox.sendKeys(args[1]+ Keys.ENTER);//hard coded value
+        Thread.sleep(3000);
         WebElement searchResult=driver.findElement(By.id("result-stats"));
         if(searchResult.isDisplayed()){
             System.out.println("Google search test pass");
@@ -27,6 +29,7 @@ public class GoogleSearch1 {
             System.out.println("Test faile");
         driver.close();
         driver.quit();
+
 
     }
 }
--- src/main/java/com/unitedcoder/uiautomation/GoogleSearch2.java
+++ src/main/java/com/unitedcoder/uiautomation/GoogleSearch2.java
@@ -1,28 +1,42 @@
 package com.unitedcoder.uiautomation;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GoogleSearch2 {
     public static void main(String[] args) {
         //define chrome driver location
         System.setProperty("webdriver.chrome.driver","c:\\webdriver\\chromedriver.exe");
         //define chrome driver object instance
-        WebDriver driver=new ChromeDriver();
+        ChromeOptions options=new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        WebDriver driver=new ChromeDriver(options);
         //maximize the browser
         driver.manage().window().maximize();
         //open google site
         driver.get("https://www.google.com");
         //find search box element
-        WebElement searchBox=driver.findElement(By.name("q"));
-        searchBox.sendKeys("Selenium"+ Keys.ENTER);
-        WebElement searchResult=driver.findElement(By.id("result-stats"));
+        //driver.findElement(By.linkText("English")).click();
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Java" + Keys.ENTER);
+        WebElement searchResult = driver.findElement(By.id("result-stats"));
         String resultText=searchResult.getText();
+        //String resultText="Yaklaşık 761.000.000 sonuç bulundu (0,37 saniye)";
+        System.out.println(resultText);
+        int beginIndex=resultText.indexOf("(");
+        resultText=resultText.substring(0,beginIndex-1);
+        resultText=resultText.replace("Yaklaşık","")
+                .replace("sonuç bulundu","").trim();
+        resultText=resultText.replace(".","");
+        if(Integer.parseInt(resultText)>=100000){
+            System.out.println("Test passed");
+        }else
+            System.out.println("Test failed");
+
         System.out.println(resultText);
         driver.close();
         driver.quit();
+
     }
 }
