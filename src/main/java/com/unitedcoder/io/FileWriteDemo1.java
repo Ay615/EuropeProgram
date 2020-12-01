57847027c6dd2f97aed74a3d65bfdb1c9b3072b3
Aynigar <aynigar615@gmail.com>
2020-12-01 22:31:45 +0300

    File

--- src/main/java/com/unitedcoder/io/FileWriteDemo1.java
+++ src/main/java/com/unitedcoder/io/FileWriteDemo1.java
@@ -5,6 +5,7 @@ public class FileWriteDemo1 {
         FileUtility fileUtility=new FileUtility();
         StringBuffer fileContant=new StringBuffer();
         fileContant.append("I love coding in Java \n");
+        //fileContant.append(System.getProperty("line.separator"));
         fileContant.append("Java coding is fun \n");
         fileContant.append("Java Test Automation Demo \n");
         fileContant.append("Test Passed");
